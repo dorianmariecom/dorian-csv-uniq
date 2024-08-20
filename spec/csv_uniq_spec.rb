3f446941d723b8b5184abd5f09b11363a6b8549b
Dorian Marié <dorian@dorianmarie.com>
2024-08-20 20:35:15 +0200

    remove dorian-times gem and related files

--- spec/csv_uniq_spec.rb
+++ spec/csv_uniq_spec.rb
@@ -3,11 +3,4 @@
 require "spec_helper"
 require "tempfile"
 
-RSpec.describe "csv-uniq" do
-  it "works" do
-    file = Tempfile.create
-    File.write(file, "id,name\n1,Dorian\n1,Damien\n2,Damien")
-    expected = "id,name\n1,Dorian\n2,Damien\n"
-    expect(`cat #{file.path} | bin/csv-uniq id --headers`).to eq(expected)
-  end
 end
