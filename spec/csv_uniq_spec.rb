6be775b483b81fa4c5d25c07b00d9e8af85e018e
Dorian Marié <dorian@dorianmarie.com>
2024-08-22 20:12:11 +0200

    add argument parsing and supporting test

---
+++ spec/csv_uniq_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "tempfile"
+
+RSpec.describe "csv-pluck" do
+  it "works" do
+    csv = Tempfile.create
+    File.write(csv, <<~CSV)
+    a,1
+    b,2
+    a,1
+    a,2
+    CSV
+    expect(`bin/csv-uniq #{csv.path}`).to eq(<<~CSV)
+    a,1
+    b,2
+    a,2
+    CSV
+  end
+end
