916c097a736a425f1e81afe361064ed7231cb94d
Dorian Marié <dorian@dorianmarie.com>
2024-08-25 20:09:23 +0200

    update configurations and dependencies

--- spec/csv_uniq_spec.rb
+++ spec/csv_uniq_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe "csv-pluck" do
   it "works" do
     csv = Tempfile.create
     File.write(csv, <<~CSV)
-    a,1
-    b,2
-    a,1
-    a,2
+      a,1
+      b,2
+      a,1
+      a,2
     CSV
     expect(`bin/csv-uniq #{csv.path}`).to eq(<<~CSV)
-    a,1
-    b,2
-    a,2
+      a,1
+      b,2
+      a,2
     CSV
   end
 end
